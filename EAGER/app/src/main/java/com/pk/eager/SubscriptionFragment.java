02eea741c30ad7235191f8c1dbd27a11c1ad6f9f
kimpham1234 <kim.g.pham@sjsu.edu>
2017-08-09 13:34:21 -0700

    stuff

--- EAGER/app/src/main/java/com/pk/eager/SubscriptionFragment.java
+++ EAGER/app/src/main/java/com/pk/eager/SubscriptionFragment.java
@@ -30,6 +30,7 @@ import com.pk.eager.adapter.NotificationRecyclerViewAdapter;
 import com.pk.eager.adapter.RecyclerTouchListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 /**
@@ -103,6 +104,7 @@ public class SubscriptionFragment extends Fragment {
                     notificationList.add(notification);
                     Log.d(TAG, "body " + notification.getBody());
                 }
+                Collections.reverse(notificationList);
                 notificationAdapter = new NotificationRecyclerViewAdapter(notificationList);
                 notificationRecyclerView.setAdapter(notificationAdapter);
 
