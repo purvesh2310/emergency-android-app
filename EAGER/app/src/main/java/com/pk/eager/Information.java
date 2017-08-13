12da904380b55ab9824b353228ae4bc8cda76701
Purvesh Kothari <purvesh2310@gmail.com>
2017-08-13 11:43:22 -0700

    Added Filter in the MapView to filter results based on the category and distance

--- EAGER/app/src/main/java/com/pk/eager/Information.java
+++ EAGER/app/src/main/java/com/pk/eager/Information.java
@@ -1,13 +1,19 @@
 package com.pk.eager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -32,6 +38,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.pk.eager.ReportObject.CompactReport;
 import com.pk.eager.util.CompactReportUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static android.content.ContentValues.TAG;
@@ -47,8 +55,21 @@ public class Information extends Fragment {
     private GoogleMap googleMap;
     private DatabaseReference db;
     LatLng currentLocation;
+    List<CompactReport> reportList;
+
+    private MenuItem mSearchAction;
+    private MenuItem mFilterAction;
+
+    private boolean isFilterApplied = false;
 
     private FusedLocationProviderClient mFusedLocationClient;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -103,6 +124,8 @@ public class Information extends Fragment {
 
                 }
 
+                reportList = new ArrayList<>();
+
                 mFusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
                             @Override
                             public void onSuccess(Location location) {
@@ -115,17 +138,10 @@ public class Information extends Fragment {
                                         @Override
                                         public void onDataChange(DataSnapshot dataSnapshot) {
                                             for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
-
                                                 CompactReport cmp = noteDataSnapshot.getValue(CompactReport.class);
-                                                Map<String, String> reportData = cmpUtil.parseReportData(cmp,"info");
-
-                                                String title = reportData.get("title");
-                                                String info = reportData.get("information");
-
-                                                googleMap.addMarker(new MarkerOptions().position(new LatLng(cmp.latitude,cmp.longitude))
-                                                        .title(title).snippet(info)
-                                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                                                reportList.add(cmp);
                                             }
+                                            addReportsToMapView(reportList);
                                         }
 
                                         @Override
@@ -133,6 +149,9 @@ public class Information extends Fragment {
                                             Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
                                         }
                                     });
+
+
+
 
                                     // For zooming automatically to the location of the marker
                                     CameraPosition cameraPosition = new CameraPosition.Builder()
@@ -143,6 +162,8 @@ public class Information extends Fragment {
                         });
             }
         });
+
+        setHasOptionsMenu(true);
 
         return rootView;
     }
@@ -173,6 +194,176 @@ public class Information extends Fragment {
 
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        mSearchAction = menu.findItem(R.id.action_search);
+        mFilterAction = menu.findItem(R.id.action_filter);
+
+        mSearchAction.setVisible(false);
+
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_filter:
+                handleMenuFilter();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void handleMenuFilter(){
+
+        if(isFilterApplied){
+            mFilterAction.setIcon(getResources().getDrawable(R.drawable.filter));
+            isFilterApplied = false;
+            googleMap.clear();
+            addReportsToMapView(reportList);
+        }else {
+            Intent intent = new Intent(getContext(), IncidentFilterActivity.class);
+            intent.putExtra("caller","MAP");
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                isFilterApplied = true;
+
+                int distance = data.getIntExtra("distance",0);
+                ArrayList<String> categoryList = data.getStringArrayListExtra("selectedCategory");
+                LatLng ll = data.getParcelableExtra("longLat_dataProvider");
+
+                if(categoryList.size()>0 && distance!=0){
+                    combineFilter(categoryList, distance, ll);
+                }else if(categoryList.size()>0){
+                    filterByCategory(categoryList);
+                }else{
+                    filterByDistance(distance, ll);
+                }
+
+                mSearchAction.setVisible(false);
+                mFilterAction.setIcon(getResources().getDrawable(R.drawable.close));
+            }
+        }
+
+    }
+
+    public void addReportsToMapView(List<CompactReport> reportList){
+
+        CompactReportUtil cmpUtil = new CompactReportUtil();
+        float markerColorType;
+
+        for(CompactReport cmpReport: reportList){
+
+            Map<String, String> reportData = cmpUtil.parseReportData(cmpReport,"info");
+
+            String title = reportData.get("title");
+            String info = reportData.get("information");
+
+            if (cmpReport.type.equals("Report")){
+                markerColorType = BitmapDescriptorFactory.HUE_RED;
+            } else{
+                markerColorType = BitmapDescriptorFactory.HUE_ORANGE;
+            }
+
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(cmpReport.latitude,cmpReport.longitude))
+                    .title(title).snippet(info)
+                    .icon(BitmapDescriptorFactory.defaultMarker(markerColorType)));
+
+        }
+    }
+
+    public void filterByCategory(List<String> categoryList){
+
+        List<CompactReport> temp = new ArrayList();
+        CompactReportUtil cmpUtil = new CompactReportUtil();
+
+        for(CompactReport cmpReport: reportList){
+
+            Map<String, String> reportData = cmpUtil.parseReportData(cmpReport,"list");
+            String reportTitle = reportData.get("title");
+
+            for (String category : categoryList) {
+
+                if (reportTitle.toLowerCase().equals(category.toLowerCase())) {
+                    temp.add(cmpReport);
+                }
+            }
+
+        }
+
+        googleMap.clear();
+        addReportsToMapView(temp);
+    }
+
+    public void filterByDistance(double queryDistance, LatLng specifiedLocation){
+
+        List<CompactReport> temp = new ArrayList();
+        CompactReportUtil cmpUtil = new CompactReportUtil();
+        double distanceInMile;
+
+        for(CompactReport cmpReport: reportList){
+            LatLng reportLocation = new LatLng(cmpReport.latitude,cmpReport.longitude);
+
+            if(specifiedLocation != null){
+                distanceInMile = cmpUtil.distanceBetweenPoints(specifiedLocation,reportLocation);
+            }else{
+                distanceInMile = cmpUtil.distanceBetweenPoints(currentLocation,reportLocation);
+            }
+
+            if (distanceInMile <= queryDistance){
+                temp.add(cmpReport);
+            }
+        }
+
+        googleMap.clear();
+        addReportsToMapView(temp);
+    }
+
+    public void combineFilter(List<String> categoryList, double queryDistance, LatLng specifiedLocation) {
+
+        List<CompactReport> temp = new ArrayList();
+        CompactReportUtil cmpUtil = new CompactReportUtil();
+        double distanceInMile;
+
+        for (CompactReport cmpReport : reportList) {
+
+            Map<String, String> reportData = cmpUtil.parseReportData(cmpReport,"list");
+            String reportTitle = reportData.get("title");
+
+            LatLng reportLocation = new LatLng(cmpReport.latitude,cmpReport.longitude);
+
+            if(specifiedLocation != null){
+                distanceInMile = cmpUtil.distanceBetweenPoints(specifiedLocation,reportLocation);
+            }else{
+                distanceInMile = cmpUtil.distanceBetweenPoints(currentLocation,reportLocation);
+            }
+
+            for (String category : categoryList) {
+                if ((reportTitle.toLowerCase().equals(category.toLowerCase())) && distanceInMile <= queryDistance) {
+                    temp.add(cmpReport);
+                }
+            }
+        }
+        googleMap.clear();
+        addReportsToMapView(temp);
     }
 }
 
