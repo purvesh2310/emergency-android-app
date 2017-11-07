a912304abf3bae4daaf810a9d6a5eb0523f0bc43
Purvesh Kothari <purvesh2310@gmail.com>
2017-11-07 11:25:45 -0800

    Customized the design of warning dialog

---
+++ EAGER/app/src/main/java/com/pk/eager/dialog/WarningDialog.java
@@ -0,0 +1,41 @@
+package com.pk.eager.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.pk.eager.R;
+
+/**
+ * Created by Purvesh on 11/6/2017.
+ */
+
+public class WarningDialog {
+
+    public void showDialog(Activity activity, String msg){
+
+        final Dialog dialog = new Dialog(activity);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_warning_report);
+
+        TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+        text.setText(msg);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
+
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+}
